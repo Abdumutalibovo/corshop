028842ea2a1c9d04df9c8cfe6db85054a10178e3
Abdumutalibovo <odilbekjuraev07@gmail.com>
2022-12-05 14:49:54 +0500

    add home screen done

--- lib/presintation/home_page.dart
+++ lib/presintation/home_page.dart
@@ -1,10 +1,54 @@
+import 'package:cars_app/view_model/cars_model_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text("Cars Item",style: TextStyle(color: Colors.black,fontSize: 20),),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InkWell(
+            onTap: (){},
+            child: Container(
+              height: 50,
+              width: double.infinity,
+              color: Colors.black54,
+            ),
+          ),
+          Consumer<CarsItemViewModel>(
+            builder: (context, counter, child) {
+              return Center(
+                child: counter.isLoading
+                    ? const CircularProgressIndicator()
+                    : (counter.carsModel == null
+                    ? const Text("Hozircha data juq")
+                    : Column(
+                  children: [
+                    Text(counter.carsModel!.id.toString()),
+                    Text(counter.carsModel!.description.toString()),
+                    Text(counter.carsModel!.establishedYear.toString()),
+                  ],
+                )),
+              );
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Provider.of<CarsItemViewModel>(context,listen: true).fetchCarInfo();
+        },
+        child: const Icon(Icons.download),
+      ),
+    );
   }
 }
